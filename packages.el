96c29574753cf06d72f7e1a3eca802967760ca43
PRD <32349731+fakeGenuis@users.noreply.github.com>
2022-06-30 17:51:44 +0800

    [add] shrface, inheribt-org

--- packages.el
+++ packages.el
@@ -29,6 +29,9 @@
 
 (package! vlf)
 
+(package! shrface)
+(package! inherit-org :recipe (:host github :repo "chenyanming/inherit-org"))
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
