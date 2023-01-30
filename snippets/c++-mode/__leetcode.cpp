460271154fa6bf3cd5cdb4f073663782c69402c2
XXX <32349731+fakeGenuis@users.noreply.github.com>
2023-01-30 22:01:54 +0800

    [update] leetcode template

--- snippets/c++-mode/__leetcode.cpp
+++ snippets/c++-mode/__leetcode.cpp
@@ -2,7 +2,9 @@
 # group: file templates
 # contributor: name
 # --
+/* `(format-time-string "%y-%m-%d %a")` */
+/* ${1:$$(yas-choose-value '("easy" "normal" "hard"))} */
 #include "header.h"
 using namespace std;
 
-$0
+${2:`(current-kill 0)`}
